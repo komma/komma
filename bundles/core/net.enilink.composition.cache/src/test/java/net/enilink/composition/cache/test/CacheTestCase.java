f9f4173e24b2016771ceb5b87180d8cbab2697ae
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2010-05-06 12:06:59 +0200

    Initial import.

---
+++ bundles/core/net.enilink.composition.cache/src/test/java/net/enilink/composition/cache/test/CacheTestCase.java
@@ -0,0 +1,138 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Fraunhofer IWU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Fraunhofer IWU - initial API and implementation
+ *******************************************************************************/
+package net.enilink.composition.cache.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import net.enilink.composition.ClassResolver;
+import net.enilink.composition.CompositionModule;
+import net.enilink.composition.DefaultObjectFactory;
+import net.enilink.composition.ObjectFactory;
+import net.enilink.composition.cache.IPropertyCache;
+import net.enilink.composition.cache.behaviours.CacheBehaviourMethodProcessor;
+import net.enilink.composition.mappers.RoleMapper;
+import net.enilink.composition.mappers.TypeFactory;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
+
+public abstract class CacheTestCase {
+	ObjectFactory<String> objectFactory;
+	ClassResolver<String> classResolver;
+	private RoleMapper<String> roleMapper;
+
+	@Before
+	public void setUp() throws Exception {
+		Injector injector = Guice.createInjector(createModule());
+		objectFactory = injector.getInstance(new Key<ObjectFactory<String>>() {
+		});
+		classResolver = injector.getInstance(new Key<ClassResolver<String>>() {
+		});
+	}
+
+	protected Module createModule() {
+		return new CompositionModule<String>() {
+			@Override
+			protected void initRoleMapper(RoleMapper<String> roleMapper,
+					TypeFactory<String> typeFactory) {
+				CacheTestCase.this.roleMapper = roleMapper;
+
+				super.initRoleMapper(roleMapper, typeFactory);
+
+				CacheTestCase.this.initRoleMapper(roleMapper);
+			}
+
+			@Override
+			protected void configure() {
+				super.configure();
+
+				bind(new Key<ObjectFactory<String>>() {
+				}).to(new TypeLiteral<DefaultObjectFactory<String>>() {
+				});
+				bind(new TypeLiteral<ClassResolver<String>>() {
+				});
+
+				getBehaviourMethodProcessorBinder().addBinding().to(
+						CacheBehaviourMethodProcessor.class).in(Singleton.class);
+			}
+
+			@Provides
+			@Singleton
+			@SuppressWarnings("unused")
+			protected TypeFactory<String> provideTypeFactory() {
+				return new TypeFactory<String>() {
+					@Override
+					public String createType(String type) {
+						return type;
+					}
+
+					@Override
+					public String toString(String type) {
+						return type;
+					}
+				};
+			}
+
+			@Provides
+			@Singleton
+			@SuppressWarnings("unused")
+			protected IPropertyCache provideCache() {
+				final Map<Object, Object> map = new HashMap<Object, Object>();
+				return new IPropertyCache() {
+					@Override
+					public Object put(Object entity, Object property,
+							Object[] parameters, Object value) {
+						List<Object> key = new ArrayList<Object>(
+								2 + parameters.length);
+						key.add(entity);
+						key.add(property);
+						for (Object parameter : parameters) {
+							key.add(parameter);
+						}
+						map.put(key, value);
+
+						return value;
+					}
+
+					@Override
+					public Object get(Object entity, Object property,
+							Object[] parameters) {
+						List<Object> key = new ArrayList<Object>(
+								2 + parameters.length);
+						key.add(entity);
+						key.add(property);
+						for (Object parameter : parameters) {
+							key.add(parameter);
+						}
+						return map.get(key);
+					}
+				};
+			}
+		};
+	}
+
+	protected void initRoleMapper(RoleMapper<String> roleMapper) {
+
+	}
+
+	protected RoleMapper<String> getRoleMapper() {
+		return roleMapper;
+	}
+}
---
+++ bundles/core/net.enilink.composition.cache/src/test/java/net/enilink/composition/cache/test/SimpleCacheTestCase.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Fraunhofer IWU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Fraunhofer IWU - initial API and implementation
+ *******************************************************************************/
+package net.enilink.composition.cache.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import net.enilink.composition.annotations.Iri;
+import net.enilink.composition.cache.annotations.Cacheable;
+import net.enilink.composition.mappers.RoleMapper;
+
+public class SimpleCacheTestCase extends CacheTestCase {
+	@Iri("urn:test:Node")
+	public interface Node<T> {
+		@Iri("urn:test:children")
+		@Cacheable
+		Set<T> getChildren(boolean x);
+	}
+
+	public static abstract class NodeSupport<T> implements Node<T> {
+		static int count = 0;
+
+		@Override
+		@Cacheable
+		public Set<T> getChildren(boolean x) {
+			count += x ? 1 : 2;
+			return new HashSet<T>();
+		}
+	}
+
+	@Override
+	protected void initRoleMapper(RoleMapper<String> roleMapper) {
+		super.initRoleMapper(roleMapper);
+
+		roleMapper.addConcept(Node.class);
+		roleMapper.addBehaviour(NodeSupport.class);
+	}
+
+	@Before
+	public void init() {
+		NodeSupport.count = 0;
+	}
+
+	@Test
+	public void testCached() throws Exception {
+		@SuppressWarnings("unchecked")
+		Node<String> node = objectFactory.createObject(Node.class);
+
+		for (int i = 0; i < 3; i++) {
+			node.getChildren(true);
+		}
+
+		Assert.assertEquals(1, NodeSupport.count);
+	}
+
+	@Test
+	public void testNotCached() throws Exception {
+		@SuppressWarnings("unchecked")
+		Node<String> node = objectFactory.createObject(Node.class);
+
+		for (int i = 0; i < 3; i++) {
+			node.getChildren(false);
+		}
+
+		Assert.assertEquals(2, NodeSupport.count);
+	}
+}
