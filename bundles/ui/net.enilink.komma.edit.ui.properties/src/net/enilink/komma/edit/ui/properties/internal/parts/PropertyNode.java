8d2033b48beeb24f19ff2581d6ba500dd9c160e6
Marko Friedemann <marko.friedemann@iwu.fraunhofer.de>
2010-06-23 09:47:35 +0200

    Skip rdf:type=owl:Thing in list to let PropertyNode show specific explicit class.
    Change-Id: I7984b41536ef4190f65abe08c8630529dc40ea7a

--- bundles/ui/net.enilink.komma.edit.ui.properties/src/net/enilink/komma/edit/ui/properties/internal/parts/PropertyNode.java
+++ bundles/ui/net.enilink.komma.edit.ui.properties/src/net/enilink/komma/edit/ui/properties/internal/parts/PropertyNode.java
@@ -10,12 +10,14 @@
  *******************************************************************************/
 package net.enilink.komma.edit.ui.properties.internal.parts;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 import net.enilink.commons.iterator.IExtendedIterator;
 import net.enilink.commons.iterator.UniqueExtendedIterator;
+import net.enilink.vocab.rdf.RDF;
 import net.enilink.komma.concepts.IProperty;
 import net.enilink.komma.concepts.IResource;
 import net.enilink.komma.core.IStatement;
@@ -45,7 +47,21 @@ public class PropertyNode {
 	public Collection<IStatement> getChildren() {
 		if (statements == null) {
 			firstStatement = null;
-			statements = getStatementIterator().toList();
+			IStatement[] stmtAry = getStatementIterator().toList().toArray(
+					new IStatement[0]);
+			// reorder first two items in case the first is rdf:type=owl:Thing
+			// and the second is not inferred
+			// this way, the expanded view is consistent with the collapsed one
+			if (stmtAry.length > 1
+					&& RDF.PROPERTY_TYPE.equals(stmtAry[0].getPredicate())
+					&& net.enilink.vocab.owl.OWL.TYPE_THING
+							.equals(stmtAry[0].getObject())
+					&& !stmtAry[1].isInferred()) {
+				IStatement typeThingStmt = stmtAry[0];
+				stmtAry[0] = stmtAry[1];
+				stmtAry[1] = typeThingStmt;
+			}
+			statements = Arrays.asList(stmtAry);
 		}
 		return statements != null ? statements : Collections
 				.<IStatement> emptyList();
@@ -59,8 +75,23 @@ public class PropertyNode {
 	public IStatement getFirstStatement() {
 		if (firstStatement == null && statements == null) {
 			IExtendedIterator<IStatement> stmtIt = getStatementIterator();
+
 			firstStatement = stmtIt.next();
 			hasMultipleStatements = stmtIt.hasNext();
+
+			// skip first statement if it's rdf:type=owl:Thing and if there are
+			// more non-inferred statements
+			// this way, the collapsed view does not show owl:Thing as the type
+			if (hasMultipleStatements
+					&& RDF.PROPERTY_TYPE.equals(firstStatement.getPredicate())
+					&& net.enilink.vocab.owl.OWL.TYPE_THING
+							.equals(firstStatement.getObject())) {
+				IStatement secondStatement = stmtIt.next();
+				if (!secondStatement.isInferred()) {
+					firstStatement = secondStatement;
+				}
+			}
+
 			stmtIt.close();
 		}
 
