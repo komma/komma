be3918cdbd05a1529952fab2eac06b77e83097bc
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2012-03-22 10:46:18 +0100

    Remove unused import.
    Change-Id: Ice4be5668c223387426d414ee686797da89b410f

--- bundles/ui/net.enilink.commons.ui/src/net/enilink/commons/ui/CommonsUi.java
+++ bundles/ui/net.enilink.commons.ui/src/net/enilink/commons/ui/CommonsUi.java
@@ -11,7 +11,6 @@
 package net.enilink.commons.ui;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
