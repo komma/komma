53c238b89e2bc294f14ecf57d76f2b6bcb22ec48
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2012-07-30 10:45:23 +0200

    Use CTRL+SPACE for content assistant in RAP.
    This change uses a new feature that enables
    RAP to transmit arbitrary keystrokes from the
    client to the server.
    
    Change-Id: I3acec753b92cf017d218c2c6340feb4a99852c0e

--- bundles/ui/net.enilink.komma.common.ui/src/net/enilink/komma/common/ui/assist/ContentProposals.java
+++ bundles/ui/net.enilink.komma.common.ui/src/net/enilink/komma/common/ui/assist/ContentProposals.java
@@ -10,8 +10,6 @@ import org.eclipse.jface.fieldassist.TextContentAdapter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Control;
 
-import net.enilink.commons.ui.CommonsUi;
-
 public class ContentProposals {
 	public static IContentProposalProvider NULL_PROPOSAL_PROVIDER = new IContentProposalProvider() {
 		public IContentProposal[] getProposals(String contents, int position) {
@@ -23,13 +21,7 @@ public class ContentProposals {
 			final Control control,
 			IContentProposalProvider contentProposalProvider,
 			char[] autoActivationCharacters) {
-		KeyStroke triggerKeyStroke;
-		if (CommonsUi.IS_RAP_RUNNING) {
-			// TODO find good trigger key stroke for RAP
-			triggerKeyStroke = null;
-		} else {
-			triggerKeyStroke = KeyStroke.getInstance(SWT.CTRL, ' ');
-		}
+		KeyStroke triggerKeyStroke = KeyStroke.getInstance(SWT.CTRL, ' ');
 
 		final IControlContentAdapter controlContentAdapter = new TextContentAdapter();
 		final ContentProposalAdapter proposalAdapter = new ContentProposalAdapter(
