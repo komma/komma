260bacd8627efbf9c9d07cdaa7549a15496fc8dd
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2012-12-21 11:33:30 +0100

    Ensure correct classpath for OSGi and non-OSGi applications.
    Change-Id: Ic8e7f5692007af52165a9c6d305eb4b1bb80e201

--- bundles/core/net.enilink.komma.model/src/main/java/net/enilink/komma/internal/model/ModelModule.java
+++ bundles/core/net.enilink.komma.model/src/main/java/net/enilink/komma/internal/model/ModelModule.java
@@ -2,19 +2,15 @@ package net.enilink.komma.internal.model;
 
 import net.enilink.komma.model.IModel;
 import net.enilink.komma.model.IModelSet;
-import net.enilink.komma.model.ModelCore;
 import net.enilink.komma.model.sesame.MemoryModelSetSupport;
 import net.enilink.komma.model.sesame.RemoteModelSetSupport;
 import net.enilink.komma.model.sesame.SerializableModelSupport;
 import net.enilink.komma.core.KommaModule;
-import net.enilink.komma.util.KommaUtil;
 import net.enilink.komma.util.RoleClassLoader;
 
 public class ModelModule extends KommaModule {
 	{
-		RoleClassLoader loader = new RoleClassLoader(this);
-		loader.load(KommaUtil.getBundleMetaInfLocations(ModelCore.PLUGIN_ID));
-
+		new RoleClassLoader(this).load();
 		addConcept(IModelSet.class);
 		addConcept(IModel.class);
 		addBehaviour(MemoryModelSetSupport.class);
