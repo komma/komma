84ab0816e48092a64c686e8b79ffad9144321649
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2013-03-22 11:09:18 +0100

    Added SPARQL helper.
    Change-Id: I1d832b9a1d5bb80b9b77f256a024fea926ca864b

---
+++ bundles/core/net.enilink.komma.em/src/main/java/net/enilink/komma/util/SparqlHelper.java
@@ -0,0 +1,48 @@
+package net.enilink.komma.util;
+
+import net.enilink.komma.core.URI;
+
+public abstract class SparqlHelper {
+	public static class Prefixes {
+		private StringBuilder sb;
+
+		private Prefixes(String prefixes) {
+			sb = new StringBuilder(prefixes);
+		}
+
+		private Prefixes() {
+			sb = new StringBuilder();
+		}
+
+		public Prefixes prefix(String prefix, URI ns) {
+			sb.append(SparqlHelper.prefix(prefix, ns.toString()));
+			return this;
+		}
+
+		public Prefixes prefix(String prefix, String ns) {
+			sb.append(SparqlHelper.prefix(prefix, ns));
+			return this;
+		}
+
+		@Override
+		public String toString() {
+			return sb.toString();
+		}
+	}
+
+	public static Prefixes defaultPrefixes() {
+		return new Prefixes(ISparqlConstants.PREFIX);
+	}
+
+	public static Prefixes emptyPrefixes() {
+		return new Prefixes();
+	}
+
+	public static String prefix(String prefix, URI ns) {
+		return prefix(prefix, ns.toString());
+	}
+
+	public static String prefix(String prefix, String ns) {
+		return "PREFIX " + prefix + ": <" + ns + "> ";
+	}
+}
