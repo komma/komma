da7c5d9a40859fd1856964c9251eb5449ea637b1
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2013-04-21 20:39:54 +0200

    Filter empty patterns and fixed var naming.
    Change-Id: I7fadf1570e3496d6175e88231be9021fa52679d4

--- bundles/core/net.enilink.komma.core/src/net/enilink/komma/core/SparqlStandardDialect.java
+++ bundles/core/net.enilink.komma.core/src/net/enilink/komma/core/SparqlStandardDialect.java
@@ -2,9 +2,7 @@ package net.enilink.komma.core;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class SparqlStandardDialect implements IDialect {
 	protected volatile int varCount = 0;
@@ -18,11 +16,24 @@ public class SparqlStandardDialect implements IDialect {
 	 * @return The variable name.
 	 */
 	protected String createVar(String prefix) {
-		return prefix + Integer.toString(varCount++, Character.MIN_RADIX);
+		return prefix + Integer.toString(varCount++, Character.MAX_RADIX);
 	}
 
 	protected String createRegexForPattern(String searchPattern, int flags) {
 		return searchPattern.replaceAll("\\*", ".*").replaceAll("\\?", ".");
+	}
+
+	protected String[] filterEmpty(String[] patterns) {
+		List<String> nonEmpty = new ArrayList<String>(patterns.length);
+		for (String pattern : patterns) {
+			if (pattern != null) {
+				pattern = pattern.trim();
+				if (pattern.length() > 0) {
+					nonEmpty.add(pattern);
+				}
+			}
+		}
+		return nonEmpty.toArray(new String[nonEmpty.size()]);
 	}
 
 	/**
@@ -34,50 +45,56 @@ public class SparqlStandardDialect implements IDialect {
 	public QueryFragment fullTextSearch(
 			Collection<? extends String> bindingNames, int flags,
 			String... patterns) {
-		LinkedHashBindings<Object> bindings = new LinkedHashBindings<Object>();
-		if ((flags & ANY) != 0) {
-			StringBuilder regex = new StringBuilder();
-			for (int i = 0; i < patterns.length; i++) {
-				String pattern = patterns[i];
-				regex.append(createRegexForPattern(pattern, flags));
-				if (i < patterns.length - 1) {
-					regex.append("|");
-				}
-			}
-			String var = createVar("pattern");
-			bindings.put(var, regex.toString());
-			StringBuilder filter = new StringBuilder();
-			for (String bindingName : bindingNames) {
-				if (filter.length() > 0) {
-					filter.append(" || ");
-				}
-				filter.append("regex(str(?" + bindingName + "), ?" + var + ")");
-			}
-			return new QueryFragment("FILTER (" + filter + ")", bindings);
-		} else {
-			List<String> vars = new ArrayList<String>();
-			for (String pattern : patterns) {
-				String var = createVar("pattern");
-				vars.add(var);
-				bindings.put(var, createRegexForPattern(pattern, flags));
-			}
-			StringBuilder filter = new StringBuilder();
-			for (String bindingName : bindingNames) {
-				if (filter.length() > 0) {
-					filter.append(" || ");
-				}
-				StringBuilder bindingFilter = new StringBuilder();
-				for (String var : vars) {
-					if (bindingFilter.length() > 0) {
-						bindingFilter.append(" && ");
+		patterns = filterEmpty(patterns);
+		if (patterns.length > 0) {
+			LinkedHashBindings<Object> bindings = new LinkedHashBindings<Object>();
+			if ((flags & ANY) != 0) {
+				StringBuilder regex = new StringBuilder();
+				for (int i = 0; i < patterns.length; i++) {
+					String pattern = patterns[i];
+					regex.append(createRegexForPattern(pattern, flags));
+					if (i < patterns.length - 1) {
+						regex.append("|");
 					}
-					bindingFilter.append("regex(str(?" + bindingName + "), ?")
-							.append(var).append(")");
 				}
-				filter.append(bindingFilter);
-			}
+				String var = createVar("pattern_");
+				bindings.put(var, regex.toString());
+				StringBuilder filter = new StringBuilder();
+				for (String bindingName : bindingNames) {
+					if (filter.length() > 0) {
+						filter.append(" || ");
+					}
+					filter.append("regex(str(?" + bindingName + "), ?" + var
+							+ ")");
+				}
+				return new QueryFragment("FILTER (" + filter + ")", bindings);
+			} else {
+				List<String> vars = new ArrayList<String>();
+				for (String pattern : patterns) {
+					String var = createVar("pattern_");
+					vars.add(var);
+					bindings.put(var, createRegexForPattern(pattern, flags));
+				}
+				StringBuilder filter = new StringBuilder();
+				for (String bindingName : bindingNames) {
+					if (filter.length() > 0) {
+						filter.append(" || ");
+					}
+					StringBuilder bindingFilter = new StringBuilder();
+					for (String var : vars) {
+						if (bindingFilter.length() > 0) {
+							bindingFilter.append(" && ");
+						}
+						bindingFilter
+								.append("regex(str(?" + bindingName + "), ?")
+								.append(var).append(")");
+					}
+					filter.append(bindingFilter);
+				}
 
-			return new QueryFragment("FILTER (" + filter + ")", bindings);
+				return new QueryFragment("FILTER (" + filter + ")", bindings);
+			}
 		}
+		return new QueryFragment("");
 	}
 }
