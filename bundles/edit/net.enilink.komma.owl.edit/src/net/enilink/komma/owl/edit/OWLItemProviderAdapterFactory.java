44b3ccc9bd75e6b455f6ddd8555ec0dc548cb112
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2014-03-05 11:30:41 +0100

    Move static methods from URIImpl to URIs.
    Change-Id: Ia6ad2d224fb3d01c69101f001ec143e6a4d0b081

--- bundles/edit/net.enilink.komma.owl.edit/src/net/enilink/komma/owl/edit/OWLItemProviderAdapterFactory.java
+++ bundles/edit/net.enilink.komma.owl.edit/src/net/enilink/komma/owl/edit/OWLItemProviderAdapterFactory.java
@@ -18,7 +18,7 @@ import net.enilink.vocab.owl.OWL;
 import net.enilink.vocab.owl.ObjectProperty;
 import net.enilink.vocab.owl.Ontology;
 import net.enilink.komma.rdfs.edit.RDFSItemProviderAdapterFactory;
-import net.enilink.komma.core.URIImpl;
+import net.enilink.komma.core.URIs;
 import net.enilink.komma.em.concepts.IClass;
 import net.enilink.komma.em.concepts.IProperty;
 import net.enilink.komma.em.concepts.IResource;
@@ -30,7 +30,7 @@ import net.enilink.komma.em.concepts.IResource;
 public class OWLItemProviderAdapterFactory extends
 		RDFSItemProviderAdapterFactory {
 	public OWLItemProviderAdapterFactory() {
-		super(OWLEditPlugin.INSTANCE, URIImpl.createURI(Class.class
+		super(OWLEditPlugin.INSTANCE, URIs.createURI(Class.class
 				.getPackage().getAnnotation(Iri.class).value()));
 	}
 
