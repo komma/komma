e17fd34f24a3e03299d5a7697d96bf5265644b4a
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2014-03-27 17:34:22 +0100

    Use owl:topObjectProperty and owl:topDataProperty as roots of property hierarchy.
    Change-Id: I40c87d15ff2a434ff52e8ea9bbd8898866accca6

--- bundles/edit/net.enilink.komma.owl.edit/src/net/enilink/komma/owl/edit/OWLPropertyItemProvider.java
+++ bundles/edit/net.enilink.komma.owl.edit/src/net/enilink/komma/owl/edit/OWLPropertyItemProvider.java
@@ -13,13 +13,54 @@ package net.enilink.komma.owl.edit;
 import java.util.Collection;
 
 import net.enilink.komma.common.util.IResourceLocator;
+import net.enilink.komma.core.URI;
 import net.enilink.komma.em.concepts.IClass;
+import net.enilink.komma.model.IObject;
 import net.enilink.komma.rdfs.edit.RDFSPropertyItemProvider;
+import net.enilink.vocab.owl.OWL;
 
 public class OWLPropertyItemProvider extends RDFSPropertyItemProvider {
 	public OWLPropertyItemProvider(
 			OWLItemProviderAdapterFactory adapterFactory,
 			IResourceLocator resourceLocator, Collection<IClass> supportedTypes) {
 		super(adapterFactory, resourceLocator, supportedTypes);
+	}
+
+	protected String findPatternsFor(URI propertyType) {
+		String typeLiteral = "<" + propertyType + ">";
+		StringBuilder patterns = new StringBuilder("?s a ").append(typeLiteral)
+				.append(" . ");
+		if (OWL.TYPE_OBJECTPROPERTY.equals(propertyType)) {
+			patterns.append("FILTER NOT EXISTS {" //
+					+ "		?s a ?otherType . ?otherType rdfs:subClassOf "
+					+ typeLiteral //
+					+ "		FILTER (?otherType = owl:AnnotationProperty || ?otherType = owl:DatatypeProperty || ?otherType = rdfs:ContainerMembershipProperty)" //
+					+ "}");
+		}
+		return patterns.toString();
+	};
+
+	@Override
+	protected String getQueryFindPatterns(Object parent) {
+		if (OWL.PROPERTY_TOPOBJECTPROPERTY.equals(parent)) {
+			return findPatternsFor(OWL.TYPE_OBJECTPROPERTY);
+		} else if (OWL.PROPERTY_TOPDATAPROPERTY.equals(parent)) {
+			return findPatternsFor(OWL.TYPE_DATATYPEPROPERTY);
+		}
+		return super.getQueryFindPatterns(parent);
+	}
+
+	@Override
+	public Collection<?> getChildren(Object object) {
+		if (object instanceof IObject) {
+			if (OWL.PROPERTY_TOPOBJECTPROPERTY.equals(object)) {
+				return ((IObject) object).getModel().getOntology()
+						.getRootObjectProperties().toList();
+			} else if (OWL.PROPERTY_TOPDATAPROPERTY.equals(object)) {
+				return ((IObject) object).getModel().getOntology()
+						.getRootDatatypeProperties().toList();
+			}
+		}
+		return super.getChildren(object);
 	}
 }
