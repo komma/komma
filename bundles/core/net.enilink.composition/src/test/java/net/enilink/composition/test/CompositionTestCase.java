c7754d9f10290ef136ad6894415e0a748358b123
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2014-06-30 18:00:20 +0200

    Remove unused imports.
    Change-Id: I44c69b86df6e7c6f1fc8d65371a51194a474aa63

--- bundles/core/net.enilink.composition/src/test/java/net/enilink/composition/test/CompositionTestCase.java
+++ bundles/core/net.enilink.composition/src/test/java/net/enilink/composition/test/CompositionTestCase.java
@@ -65,7 +65,6 @@ public abstract class CompositionTestCase {
 
 			@Provides
 			@Singleton
-			@SuppressWarnings("unused")
 			protected TypeFactory<String> provideTypeFactory() {
 				return new TypeFactory<String>() {
 					@Override
