d226a426a026beeab716a8fe27e5148020423dbc
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2015-02-10 17:56:47 +0100

    Track client threads for debugging purposes.
    Change-Id: Ie0915515fb8d5d9fe7a19b272d63d6688bb8cb1a

--- bundles/core/net.enilink.komma.em/src/main/java/net/enilink/komma/em/util/UnitOfWork.java
+++ bundles/core/net.enilink.komma.em/src/main/java/net/enilink/komma/em/util/UnitOfWork.java
@@ -1,13 +1,30 @@
 package net.enilink.komma.em.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import net.enilink.komma.core.IUnitOfWork;
 
 public class UnitOfWork implements IUnitOfWork {
 	private ThreadLocal<Integer> countLocal = new ThreadLocal<>();
 	private ThreadLocal<List<AutoCloseable>> trackedCloseablesLocal = new ThreadLocal<>();
+
+	// track client threads for debugging purposes
+	private Set<Thread> clientThreads = Collections
+			.newSetFromMap(new ConcurrentHashMap<Thread, Boolean>());
+	private final boolean debug;
+
+	public UnitOfWork() {
+		this(false);
+	}
+
+	public UnitOfWork(boolean debug) {
+		this.debug = debug;
+	}
 
 	public void addCloseable(AutoCloseable closeable) {
 		List<AutoCloseable> trackedCloseables = trackedCloseablesLocal.get();
@@ -22,6 +39,9 @@ public class UnitOfWork implements IUnitOfWork {
 	public void begin() {
 		Integer count = countLocal.get();
 		countLocal.set(count == null ? 1 : count + 1);
+		if (debug) {
+			clientThreads.add(Thread.currentThread());
+		}
 	}
 
 	@Override
@@ -47,6 +67,10 @@ public class UnitOfWork implements IUnitOfWork {
 			}
 			trackedCloseablesLocal.remove();
 			countLocal.remove();
+
+			if (debug) {
+				clientThreads.remove(Thread.currentThread());
+			}
 		}
 	}
 
@@ -55,5 +79,9 @@ public class UnitOfWork implements IUnitOfWork {
 	 */
 	public boolean isActive() {
 		return countLocal.get() != null;
+	}
+
+	public Collection<Thread> getClientThreads() {
+		return Collections.unmodifiableCollection(clientThreads);
 	}
 }
