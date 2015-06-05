08c9442b13eb8ce81c8af9c0fdd9a422f6505281
Heiner Reinhardt <heiner.reinhardt@googlemail.com>
2015-06-05 18:00:53 +0200

    Refactoring
    Change-Id: Ie9d972b4cd0c9ac7263c1f9774cc8fa656451152

--- bundles/core/net.enilink.commons.logging/src/net/enilink/commons/logging/LoggingPlugin.java
+++ bundles/core/net.enilink.commons.logging/src/net/enilink/commons/logging/LoggingPlugin.java
@@ -10,12 +10,13 @@
  *******************************************************************************/
 package net.enilink.commons.logging;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.io.PrintStream;
 
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Plugin;
-import org.eclipse.osgi.service.datalocation.Location;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,6 +37,8 @@ public class LoggingPlugin extends Plugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "net.enilink.commons.logging";
 
+	private static final String LOGBACK_CONFIG_FILE = "logback.xml";
+
 	// The shared instance
 	private static LoggingPlugin plugin;
 
@@ -55,63 +58,97 @@ public class LoggingPlugin extends Plugin {
 
 	public static void init() {
 		if (!initialized) {
-
 			// assume SLF4J is bound to logback in the current environment
 			LoggerContext context = (LoggerContext) LoggerFactory
 					.getILoggerFactory();
-
-			try {
-				JoranConfigurator configurator = new JoranConfigurator();
-				configurator.setContext(context);
-				// Call context.reset() to clear any previous configuration,
-				// e.g. default configuration. For multi-step configuration,
-				// omit calling context.reset().
-				context.reset();
-
-				String fileName = "logback.xml";
-				// try to load config file from workspace; otherwise the default
-				// from this plugin will be used
-				Location instanceLocation = Platform.isRunning() ? Platform
-						.getInstanceLocation() : null;
-				File configFile = null;
-				if (instanceLocation != null) {
-					configFile = new File(instanceLocation.getURL().getPath()
-							+ fileName);
-				}
-				if (configFile != null && configFile.exists()) {
-					// use file from workspace
-					configurator.doConfigure(configFile);
-				} else {
-					// use file from classpath
-					configurator.doConfigure(LoggingPlugin.class
-							.getResource(fileName));
-				}
-			} catch (JoranException je) {
-				// StatusPrinter will handle this
-			}
+			loadConfiguration(context);
 			StatusPrinter.printInCaseOfErrorsOrWarnings(context);
-			if ("true"
-					.equals(System
-							.getProperty("net.enilink.commons.logging.captureSystemOut"))) {
-				// redirect system output output to log.info()
-				System.setOut(new PrintStream(System.out) {
-					private Logger log = LoggerFactory.getLogger(System.class);
-
-					public void print(String message) {
-						log.info(message);
-					}
-				});
-
-				// redirect system error output to log.error()
-				System.setErr(new PrintStream(System.err) {
-					private Logger log = LoggerFactory.getLogger(System.class);
-
-					public void print(String message) {
-						log.error(message);
-					}
-				});
+			if (isTrue("net.enilink.logger.captureSystemOut")) {
+				redirectStdoutToLogInfo();
+				redirectStdoutToLogError();
 			}
 			initialized = true;
+		}
+	}
+
+	private static void loadConfiguration(LoggerContext context) {
+		try {
+			JoranConfigurator configurator = new JoranConfigurator();
+			configurator.setContext(context);
+			// Call context.reset() to clear any previous configuration,
+			// e.g. default configuration. For multi-step configuration,
+			// omit calling context.reset().
+			context.reset();
+
+			InputStream config = getLoggingFileFromWorkspace();
+			if (config == null) {
+				config = getLoggingFileFromInstallationPath();
+			}
+			if (config == null) {
+				config = getLoggingFileFromClasspath();
+			}
+			if (config != null) {
+				configurator.doConfigure(config);
+			}
+
+		} catch (JoranException je) {
+			// StatusPrinter will handle this
+		}
+	}
+
+	private static boolean isTrue(String property) {
+		return "true".equals(System.getProperty(property));
+	}
+
+	private static void redirectStdoutToLogError() {
+		System.setErr(new PrintStream(System.err) {
+			private Logger log = LoggerFactory.getLogger(System.class);
+
+			public void print(String message) {
+				log.error(message);
+			}
+		});
+	}
+
+	private static void redirectStdoutToLogInfo() {
+		System.setOut(new PrintStream(System.out) {
+			private Logger log = LoggerFactory.getLogger(System.class);
+
+			public void print(String message) {
+				log.info(message);
+			}
+		});
+	}
+
+	private static InputStream getLoggingFileFromClasspath() {
+		return LoggingPlugin.class.getResourceAsStream(LOGBACK_CONFIG_FILE);
+	}
+
+	private static InputStream getLoggingFileFromWorkspace() {
+		if (Platform.isRunning()) {
+			String path = Platform.getInstanceLocation().getURL().getPath()
+					+ LOGBACK_CONFIG_FILE;
+			try {
+				return new FileInputStream(path);
+			} catch (FileNotFoundException e) {
+				return null;
+			}
+		} else {
+			return null;
+		}
+	}
+
+	private static InputStream getLoggingFileFromInstallationPath() {
+		if (Platform.isRunning()) {
+			String path = Platform.getInstallLocation().getURL().getPath()
+					+ LOGBACK_CONFIG_FILE;
+			try {
+				return new FileInputStream(path);
+			} catch (FileNotFoundException e) {
+				return null;
+			}
+		} else {
+			return null;
 		}
 	}
 
