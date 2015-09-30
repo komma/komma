a68e6b47d9c32a709214380eba9b77817fe0a023
Marko Friedemann <marko.friedemann@iwu.fraunhofer.de>
2015-09-30 19:06:30 +0200

    Save() the workspace in komma.model plugin's stop().

--- bundles/core/net.enilink.komma.model/src/main/java/net/enilink/komma/model/ModelPlugin.java
+++ bundles/core/net.enilink.komma.model/src/main/java/net/enilink/komma/model/ModelPlugin.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
@@ -21,12 +22,14 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.osgi.framework.BundleContext;
 
 import net.enilink.commons.util.extensions.RegistryFactoryHelper;
 import net.enilink.komma.common.AbstractKommaPlugin;
 import net.enilink.komma.common.util.IResourceLocator;
+import net.enilink.komma.core.KommaModule;
 import net.enilink.komma.internal.model.Messages;
 import net.enilink.komma.internal.model.ModelModule;
 import net.enilink.komma.internal.model.extensions.ContentFactoriesRegistryReader;
@@ -43,7 +46,6 @@ import net.enilink.komma.model.base.URIMapRuleSet;
 import net.enilink.komma.model.validation.Diagnostician;
 import net.enilink.komma.model.validation.IValidator;
 import net.enilink.komma.model.validation.ValidatorRegistry;
-import net.enilink.komma.core.KommaModule;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -258,6 +260,23 @@ public class ModelPlugin extends AbstractKommaPlugin {
 				workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
 			}
 			readExtensions();
+		}
+
+		@Override
+		public void stop(BundleContext ctx) throws Exception {
+			if (IS_RESOURCES_BUNDLE_AVAILABLE) {
+				IStatus status;
+				try {
+					IWorkspace ws = workspaceRoot.getWorkspace();
+					status = ws.save(true, new NullProgressMonitor());
+				} catch (CoreException e) {
+					status = e.getStatus();
+				}
+				if (!status.isOK()) {
+					logErrorStatus("workspace could not be saved", status);
+				}
+			}
+			super.stop(ctx);
 		}
 	}
 
