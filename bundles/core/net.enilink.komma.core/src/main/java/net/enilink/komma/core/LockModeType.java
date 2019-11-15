fb0bd1b640de1a9e9d68c27fba6eb592f6fc807c
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2019-11-15 19:17:23 +0100

    Update licensing information to EPL v2.0.

--- bundles/core/net.enilink.komma.core/src/main/java/net/enilink/komma/core/LockModeType.java
+++ bundles/core/net.enilink.komma.core/src/main/java/net/enilink/komma/core/LockModeType.java
@@ -2,11 +2,8 @@
  * Copyright (c) 1998, 2010 Oracle. All rights reserved. 
  * 
  * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
- * which accompanies this distribution. 
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
- * http://www.eclipse.org/org/documents/edl-v10.php.
+ * terms of the Eclipse Public License v2.0. 
+ * The Eclipse Public License is available at https://www.eclipse.org/legal/epl-2.0/.
  * 
  * Contributors:
  *     dclarke - Java Persistence 2.0 - Proposed Final Draft (March 13, 2009)
