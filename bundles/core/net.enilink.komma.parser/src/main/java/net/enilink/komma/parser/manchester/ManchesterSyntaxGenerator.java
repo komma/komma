b9dc46b3d1c6dc95d4dc84fba783908d4115fca7
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2020-01-08 17:22:27 +0100

    Extended Manchester Syntax Generator.

--- bundles/core/net.enilink.komma.parser/src/main/java/net/enilink/komma/parser/manchester/ManchesterSyntaxGenerator.java
+++ bundles/core/net.enilink.komma.parser/src/main/java/net/enilink/komma/parser/manchester/ManchesterSyntaxGenerator.java
@@ -1,22 +1,32 @@
 package net.enilink.komma.parser.manchester;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
+import net.enilink.commons.iterator.IExtendedIterator;
+import net.enilink.komma.core.IBindings;
+import net.enilink.komma.core.IEntity;
+import net.enilink.komma.core.IEntityManager;
+import net.enilink.komma.core.ILiteral;
+import net.enilink.komma.core.INamespace;
+import net.enilink.komma.core.IQuery;
+import net.enilink.komma.core.IReference;
+import net.enilink.komma.core.URI;
 import net.enilink.vocab.owl.DataRange;
+import net.enilink.vocab.owl.OWL;
+import net.enilink.vocab.owl.Ontology;
 import net.enilink.vocab.owl.Restriction;
 import net.enilink.vocab.rdf.RDF;
 import net.enilink.vocab.rdfs.Class;
 import net.enilink.vocab.rdfs.Datatype;
 import net.enilink.vocab.xmlschema.XMLSCHEMA;
-import net.enilink.komma.core.IBindings;
-import net.enilink.komma.core.IEntity;
-import net.enilink.komma.core.ILiteral;
-import net.enilink.komma.core.IReference;
-import net.enilink.komma.core.URI;
 
 /**
  * Generator for the
@@ -24,9 +34,40 @@ import net.enilink.komma.core.URI;
  * Syntax</a>.
  */
 public class ManchesterSyntaxGenerator {
+	protected static final Comparator<? super IReference> REF_COMPARATOR = new Comparator<IReference>() {
+		@Override
+		public int compare(IReference o1, IReference o2) {
+			if (o1 == null) {
+				if (o2 == null) {
+					return 0;
+				}
+				return -1;
+			} else if (o2 == null) {
+				return 1;
+			}
+			return o1.toString().compareTo(o2.toString());
+		}
+	};
+
+	protected int indent = 0;
+	protected static final String SPARQL_PREFIX = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX owl: <http://www.w3.org/2002/07/owl#> ";
 	static final String FACET_QUERY = createFacetQuery();
 
-	private static String createFacetQuery() {
+	protected void indent() {
+		indent += 2;
+	}
+
+	protected void dedent() {
+		indent -= 2;
+	}
+
+	protected void withIndent(Runnable runnable) {
+		indent();
+		runnable.run();
+		dedent();
+	}
+
+	protected static String createFacetQuery() {
 		StringBuilder sb = new StringBuilder("PREFIX xsd: <" + XMLSCHEMA.NAMESPACE + ">\n");
 		sb.append("select ?facet ?value where {\n");
 		Iterator<String> facets = Arrays.asList("length", "minLength", "maxLength", "pattern", "langPattern",
@@ -61,13 +102,135 @@ public class ManchesterSyntaxGenerator {
 	}
 
 	protected ManchesterSyntaxGenerator append(Object token) {
+		if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '\n') {
+			for (int i = 0; i < indent; i++) {
+				sb.append(' ');
+			}
+		}
 		sb.append(token);
 		return this;
 	}
 
-	private StringBuilder sb = new StringBuilder();
+	protected StringBuilder sb = new StringBuilder();
 
-	private ManchesterSyntaxGenerator clazz(Class clazz, int prio) {
+	public String ontologyDocument(Ontology ontology) {
+		IEntityManager em = ontology.getEntityManager();
+		try (IExtendedIterator<INamespace> it = em.getNamespaces()) {
+			if (it.hasNext()) {
+				it.forEach(ns -> prefix(ns));
+			}
+			append("\n");
+		}
+
+		ontology(ontology);
+
+		IQuery<?> query = em.createQuery(SPARQL_PREFIX + //
+				"SELECT DISTINCT ?class WHERE { " + //
+				"{ ?class a rdfs:Class } UNION { ?class a owl:Class } " + //
+				"graph ?g { ?class ?someP ?someO }" + //
+				"FILTER isIRI(?class) . " + //
+				"} ORDER BY ?class");
+		query.setParameter("g", ontology);
+		try (IExtendedIterator<Class> it = query.evaluate(Class.class)) {
+			it.forEach(this::classDefinition);
+		}
+		return sb.toString();
+	}
+
+	protected ManchesterSyntaxGenerator prefix(INamespace ns) {
+		append("Prefix: " + ns.getPrefix() + ":");
+		value(ns.getURI());
+		return append("\n");
+	}
+
+	protected ManchesterSyntaxGenerator ontology(Ontology ontology) {
+		append("Ontology: ");
+		value(ontology);
+		append("\n");
+		withIndent(() -> {
+			ontology.getOwlImports().stream().sorted(REF_COMPARATOR).forEachOrdered(this::ontologyImport);
+		});
+		return append("\n");
+	}
+
+	protected ManchesterSyntaxGenerator ontologyImport(Ontology ontology) {
+		append("Import: ");
+		value(ontology);
+		return append("\n");
+	}
+
+	protected ManchesterSyntaxGenerator classDefinition(Class clazz) {
+		append("Class: ");
+		value(clazz);
+		append("\n");
+
+		withIndent(() -> {
+			classŔelation("SubClassOf", clazz.getRdfsSubClassOf());
+			classŔelation("EquivalentTo", ((net.enilink.vocab.owl.Class) clazz).getOwlEquivalentClasses());
+			classŔelation("DisjointWith", ((net.enilink.vocab.owl.Class) clazz).getOwlDisjointWith());
+			// classŔelation("DisjointUnionOf",
+			// ((net.enilink.vocab.owl.Class)clazz).getOwlDisjointUnionOf());
+		});
+		classDefinitionExt(clazz);
+		return this;
+	}
+
+	protected void classŔelation(String key, Collection<? extends Class> elements) {
+		List<Class> named = new ArrayList<>();
+		List<Class> anonymous = new ArrayList<>();
+		Map<IReference, List<Restriction>> restrictions = new HashMap<>();
+		elements.forEach(c -> {
+			if (OWL.TYPE_THING.equals(c)) {
+				// ignore, this is implicit knowledge
+			} else if (c.getURI() != null) {
+				named.add(c);
+			} else {
+				if (c instanceof Restriction) {
+					Restriction r = (Restriction) c;
+					// group by property
+					restrictions.compute(r.getOwlOnProperty(), (k, v) -> {
+						if (v == null) {
+							v = new ArrayList<>();
+						}
+						v.add(r);
+						return v;
+					});
+				} else {
+					anonymous.add(c);
+				}
+			}
+		});
+
+		appendKey(key, named, this::clazz);
+		appendKey(key, anonymous, this::clazz);
+		restrictions.entrySet().stream().sorted((a, b) -> REF_COMPARATOR.compare(a.getKey(), b.getKey()))
+				.forEachOrdered(entry -> {
+					appendKey(key, entry.getValue(), this::clazz);
+				});
+	}
+
+	private <T> void appendKey(String key, List<T> elements, Consumer<T> func) {
+		if (!elements.isEmpty()) {
+			append(key + ": ");
+			Iterator<T> it = elements.iterator();
+			while (it.hasNext()) {
+				func.accept(it.next());
+				if (it.hasNext()) {
+					append(", ");
+				}
+			}
+			append("\n");
+		}
+	}
+
+	protected void classDefinitionExt(Class clazz) {
+	}
+
+	protected ManchesterSyntaxGenerator clazz(Class clazz) {
+		return clazz(clazz, 0);
+	}
+
+	protected ManchesterSyntaxGenerator clazz(Class clazz, int prio) {
 		if (clazz.getURI() == null) {
 			if (clazz instanceof Restriction) {
 				return restriction((Restriction) clazz, prio);
@@ -93,18 +256,18 @@ public class ManchesterSyntaxGenerator {
 	}
 
 	/**
-	 * Converts a list of datatype restrictions to a Manchester expression in
-	 * the form [facet1 value1, facet2 value2, ...]
+	 * Converts a list of datatype restrictions to a Manchester expression in the
+	 * form [facet1 value1, facet2 value2, ...]
 	 * 
 	 * Example: xsd:int[>=18]
 	 * 
 	 * @param list
-	 *            The list of datatype restrictions which are expressed using
-	 *            XML Schmema facets.
+	 *            The list of datatype restrictions which are expressed using XML
+	 *            Schema facets.
 	 * 
 	 * @return The generator instance.
 	 */
-	private ManchesterSyntaxGenerator datatypeRestrictions(List<?> list) {
+	protected ManchesterSyntaxGenerator datatypeRestrictions(List<?> list) {
 		if (list != null) {
 			append("[");
 			Iterator<? extends Object> it = list.iterator();
@@ -128,11 +291,11 @@ public class ManchesterSyntaxGenerator {
 		return this;
 	}
 
-	private ManchesterSyntaxGenerator dataRange(DataRange dataRange) {
+	protected ManchesterSyntaxGenerator dataRange(DataRange dataRange) {
 		return clazz(dataRange, 0);
 	}
 
-	private ManchesterSyntaxGenerator list(List<? extends Object> list) {
+	private ManchesterSyntaxGenerator list(Collection<? extends Object> list) {
 		append("{");
 		Iterator<? extends Object> it = list.iterator();
 		while (it.hasNext()) {
@@ -145,7 +308,7 @@ public class ManchesterSyntaxGenerator {
 		return this;
 	}
 
-	private ManchesterSyntaxGenerator onClassOrDataRange(Restriction restriction) {
+	protected ManchesterSyntaxGenerator onClassOrDataRange(Restriction restriction) {
 		if (restriction.getOwlOnClass() != null) {
 			return clazz(restriction.getOwlOnClass(), 0);
 		} else if (restriction.getOwlOnDataRange() != null) {
@@ -215,7 +378,7 @@ public class ManchesterSyntaxGenerator {
 		return value(restriction);
 	}
 
-	private ManchesterSyntaxGenerator setOfClasses(List<? extends Class> set, String operator, int operatorPrio,
+	protected ManchesterSyntaxGenerator setOfClasses(List<? extends Class> set, String operator, int operatorPrio,
 			int prio) {
 		Iterator<? extends Class> it = set.iterator();
 		if (operatorPrio < prio && set.size() > 1) {
