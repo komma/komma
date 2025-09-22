c14b799b939de5cf19eba27585e7430493fc2dab
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2025-09-22 13:37:17 +0200

    Fix release.sh to not override non-matching versions.

--- releng/release.sh
+++ releng/release.sh
@@ -23,11 +23,11 @@ esac
 shift # past argument or value
 done
 
-mvn versions:set -DnewVersion=$RELEASEVERSION -DgroupId='*' -DartifactId='*' -DprocessAllModules -DgenerateBackupPoms=false -P versions
+mvn versions:set -DnewVersion=$RELEASEVERSION -DgroupId='*' -DartifactId='*' -DgenerateBackupPoms=false -P versions
 
 git commit -a -m "prepare release $RELEASEVERSION"
 git tag -a v$RELEASEVERSION -m "KOMMA release $RELEASEVERSION"
 
-mvn versions:set -DnewVersion=$DEVELOPMENTVERSION -DgroupId='*' -DartifactId='*' -DprocessAllModules -DgenerateBackupPoms=false -P versions
+mvn versions:set -DnewVersion=$DEVELOPMENTVERSION -DgroupId='*' -DartifactId='*' -DgenerateBackupPoms=false -P versions
 
 git commit -a -m "prepare for next development iteration"
